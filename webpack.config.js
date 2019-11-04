4d495d625f7aeca00dd26ad960d97b66493a15bc
chuhx <chuhx@yonyou.com>
2019-11-04 20:51:53 +0800

    webpack起步 使用了 html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: "production",
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[chunkHash:8].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+}

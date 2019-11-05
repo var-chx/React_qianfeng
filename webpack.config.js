c599a1dcf6dec0fdb65b288cbd202674773fb4e4
chuhx <chuhx@yonyou.com>
2019-11-05 14:40:12 +0800

    webpack配置中使用 css抽取插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 const path = require('path')
+//自动生成 html的webpack 插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 抽离 css文件的 webpack插件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     mode: "production",
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].[chunkHash:8].js"
+        filename: "js/[name].[chunkHash:8].js"
     },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[chunkHash:8].css',
+          chunkFilename: '[id].css',
+          ignoreOrder: false, // Enable to remove warnings about conflicting order
+        })
     ],
     module: {
         rules: [
           {
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
           },
         ],
     }

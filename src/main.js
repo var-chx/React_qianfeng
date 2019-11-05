a656d574912e2e4def8afa7fdc2a26acb19b59f6
chuwx <var_chx@126.com>
2019-11-06 00:15:13 +0800

    项目中添加babel-loader 转ES5

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import './index.css'
 import './index.less'
+import { add } from './test'
 console.log("HelloWord")
 console.log("我使用webpack-dev-server热更新成功了!")
+add(1, 2)
+let arr = [1, 2, 3]
+console.log(...arr)
+
--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,16 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
         ],
     },
     devServer: {
